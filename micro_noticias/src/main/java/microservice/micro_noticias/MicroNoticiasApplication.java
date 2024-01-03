74e8e9c01e9e14b895d5036af4453dc4a8cd6f95
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-03 16:00:26 -0400

    CHORE: add primeiro Microsserviço, Notícias

---
+++ micro_noticias/src/main/java/microservice/micro_noticias/MicroNoticiasApplication.java
@@ -0,0 +1,13 @@
+package microservice.micro_noticias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroNoticiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroNoticiasApplication.class, args);
+	}
+
+}
---
+++ micro_noticias/src/test/java/microservice/micro_noticias/MicroNoticiasApplicationTests.java
@@ -0,0 +1,13 @@
+package microservice.micro_noticias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroNoticiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
