b849f5029f49672efadda729332b0df22fc10422
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-03 16:03:16 -0400

    CHORE: add primeiro microservice, notícias

--- micronoticias/src/test/java/microservice/micronoticias/MicronoticiasApplicationTests.java
+++ micronoticias/src/test/java/microservice/micronoticias/MicronoticiasApplicationTests.java
@@ -1,10 +1,10 @@
-package microservice.micro_noticias;
+package microservice.micronoticias;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroNoticiasApplicationTests {
+class MicronoticiasApplicationTests {
 
 	@Test
 	void contextLoads() {
