aa9d5f35bb78498e2a50f56786426b62c80e0946
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-09 06:54:19 -0400

    FEAT: add Log in Controller

--- micronoticias/src/main/java/microservice/micronoticias/adapter/in/controller/NoticiaController.java
+++ micronoticias/src/main/java/microservice/micronoticias/adapter/in/controller/NoticiaController.java
@@ -2,6 +2,7 @@ package microservice.micronoticias.adapter.in.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import microservice.micronoticias.adapter.in.dto.request.NoticiaCadastrarDtoIn;
 import microservice.micronoticias.adapter.in.dto.response.NoticiaCadastrarDtoOut;
 import microservice.micronoticias.adapter.in.mapper.NoticiaMapperIn;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.URI;
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/api/v1/noticias")
 @RequiredArgsConstructor
@@ -30,11 +32,15 @@ public class NoticiaController {
         produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<NoticiaCadastrarDtoOut> cadastrar(@RequestBody @Valid NoticiaCadastrarDtoIn cadastrarDtoIn) {
 
+        log.info("Recebida requisição para criar Notícia: {}", cadastrarDtoIn.titulo());
+
         var resposta = Optional.of(cadastrarDtoIn)
             .map(this.mapperIn::toNoticia)
             .map(this.cadastrarInputPort::cadastrar)
             .map(this.mapperIn::toNoticiaCadastrarDtoOut)
             .orElseThrow();
+
+        log.info("Notícia criada com sucesso: {}", resposta.titulo());
 
         return ResponseEntity
             .created(URI.create("/api/v1/noticias/" + resposta.id()))
