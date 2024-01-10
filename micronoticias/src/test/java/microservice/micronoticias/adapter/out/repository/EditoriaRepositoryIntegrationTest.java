fae6232dc5676eeda190ec3b871ef0867102fa8e
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-10 11:33:25 -0400

    FEAT: add

--- micronoticias/src/test/java/microservice/micronoticias/adapter/out/repository/EditoriaRepositoryIntegrationTest.java
+++ micronoticias/src/test/java/microservice/micronoticias/adapter/out/repository/EditoriaRepositoryIntegrationTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @DataJpaTest
 @DisplayName("Integration Repository - Editoria")
 class EditoriaRepositoryIntegrationTest {
