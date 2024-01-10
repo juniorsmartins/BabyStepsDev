e8f9df255c91ecd91083a55efee7d9ed7949b35f
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-10 07:36:31 -0400

    ...

--- micronoticias/src/main/java/microservice/micronoticias/adapter/in/controller/NoticiaController.java
+++ micronoticias/src/main/java/microservice/micronoticias/adapter/in/controller/NoticiaController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
