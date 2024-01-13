2bef14667b159b3233671da8b4256d1ac52bf909
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-13 15:33:54 -0400

    TEST: add

--- micronoticias/src/test/java/microservice/micronoticias/adapter/in/dto/request/EditoriaCriarDtoInUnitTest.java
+++ micronoticias/src/test/java/microservice/micronoticias/adapter/in/dto/request/EditoriaCriarDtoInUnitTest.java
@@ -3,11 +3,10 @@ package microservice.micronoticias.adapter.in.dto.request;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import microservice.micronoticias.utility.FactoryObjectMother;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -41,6 +40,74 @@ class EditoriaCriarDtoInUnitTest {
         Assertions.assertTrue(violations.isEmpty());
     }
 
+    @Nested
+    @DisplayName("Nomenclatura")
+    class Nomenclatura {
 
+        @Test
+        @DisplayName("nulo")
+        void dadoNomenclaturaNula_QuandoInstanciar_EntaoLancarException() {
+            var dtoIn = editoriaCriarDtoInBuilder.nomenclatura(null).build();
+            Set<ConstraintViolation<EditoriaCriarDtoIn>> violations = validator.validate(dtoIn);
+            Assertions.assertFalse(violations.isEmpty());
+            Assertions.assertEquals(1, violations.size());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"", "   "})
+        @DisplayName("vazio ou em branco")
+        void dadoNomenclaturaVaziaOuEmBranco_QuandoInstanciar_EntaoLancarException(String valor) {
+            var dtoIn = editoriaCriarDtoInBuilder.nomenclatura(valor).build();
+            Set<ConstraintViolation<EditoriaCriarDtoIn>> violations = validator.validate(dtoIn);
+            Assertions.assertFalse(violations.isEmpty());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+            "2_",
+            "101 RegraDeLimiteMaximoDeCaracteres RegraDeLimiteMaximoDeCaracteres RegraDeLimiteMaximoDeCaracteres__"})
+        @DisplayName("com tamanho inválido")
+        void dadoNomenclaturaComTamanhoInvalido_QuandoInstanciar_EntaoLancarException(String valor) {
+            var dtoIn = editoriaCriarDtoInBuilder.nomenclatura(valor).build();
+            Set<ConstraintViolation<EditoriaCriarDtoIn>> violations = validator.validate(dtoIn);
+            Assertions.assertFalse(violations.isEmpty());
+            Assertions.assertEquals(1, violations.size());
+        }
+    }
+
+    @Nested
+    @DisplayName("Descrição")
+    class Descricao {
+
+        @Test
+        @DisplayName("nulo")
+        void dadoDescricaoNula_QuandoInstanciar_EntaoLancarException() {
+            var dtoIn = editoriaCriarDtoInBuilder.descricao(null).build();
+            Set<ConstraintViolation<EditoriaCriarDtoIn>> violations = validator.validate(dtoIn);
+            Assertions.assertFalse(violations.isEmpty());
+            Assertions.assertEquals(1, violations.size());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"", "   "})
+        @DisplayName("vazio ou em branco")
+        void dadoDescricaoVaziaOuEmBranco_QuandoInstanciar_EntaoLancarException(String valor) {
+            var dtoIn = editoriaCriarDtoInBuilder.descricao(valor).build();
+            Set<ConstraintViolation<EditoriaCriarDtoIn>> violations = validator.validate(dtoIn);
+            Assertions.assertFalse(violations.isEmpty());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+            "9 RegraDe",
+            "201 RegraDeLimiteMaximoDeCaracteres RegraDeLimiteMaximoDeCaracteres RegraDeLimiteMaximoDeCaracteres RegraDeLimiteMaximoDeCaracteres RegraDeLimiteMaximoDeCaracteres RegraDeLimiteMaximoDeCaracteres Regra"})
+        @DisplayName("com tamanho inválido")
+        void dadoDescricaoComTamanhoInvalido_QuandoInstanciar_EntaoLancarException(String valor) {
+            var dtoIn = editoriaCriarDtoInBuilder.descricao(valor).build();
+            Set<ConstraintViolation<EditoriaCriarDtoIn>> violations = validator.validate(dtoIn);
+            Assertions.assertFalse(violations.isEmpty());
+            Assertions.assertEquals(1, violations.size());
+        }
+    }
 }
 
