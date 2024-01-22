a4c0095df590ca8f16f0e7f32a209a69210e24fe
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-22 14:10:33 -0400

    FEAT: add LockOptimistic

--- micronoticias/src/test/resources/sql/editorias/editorias-insert.sql
+++ micronoticias/src/test/resources/sql/editorias/editorias-insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO editorias(id, nomenclatura, descricao) VALUES (1001, 'Economia', 'Informações sobre a economia do país e mundo.');
-INSERT INTO editorias(id, nomenclatura, descricao) VALUES (1002, 'Tecnologia', 'Informações sobre a tecnologia no país e mundo.');
+INSERT INTO editorias(id, version, nomenclatura, descricao) VALUES (1001, '2023-01-20 12:25:34', 'Economia', 'Informações sobre a economia do país e mundo.');
+INSERT INTO editorias(id, version, nomenclatura, descricao) VALUES (1002, '2024-01-12 10:08:11', 'Tecnologia', 'Informações sobre a tecnologia no país e mundo.');
 
