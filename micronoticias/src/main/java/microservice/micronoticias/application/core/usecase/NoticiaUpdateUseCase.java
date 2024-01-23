8608e876c5559e7dd80ffb69724a48deddf8c1ef
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-23 13:31:09 -0400

    FEAT: add UseCase

--- micronoticias/src/main/java/microservice/micronoticias/application/core/usecase/NoticiaUpdateUseCase.java
+++ micronoticias/src/main/java/microservice/micronoticias/application/core/usecase/NoticiaUpdateUseCase.java
@@ -3,8 +3,15 @@ package microservice.micronoticias.application.core.usecase;
 import microservice.micronoticias.application.core.domain.Noticia;
 import microservice.micronoticias.application.port.input.NoticiaUpdateInputPort;
 import microservice.micronoticias.application.port.output.NoticiaUpdateOutputPort;
+import microservice.micronoticias.config.exception.http_500.NoticiaUpdateUseCaseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 public class NoticiaUpdateUseCase implements NoticiaUpdateInputPort {
+
+    private static final Logger log = LoggerFactory.getLogger(NoticiaUpdateUseCase.class);
 
     private final NoticiaUpdateOutputPort noticiaUpdateOutputPort;
 
@@ -15,7 +22,15 @@ public class NoticiaUpdateUseCase implements NoticiaUpdateInputPort {
     @Override
     public Noticia update(Noticia noticia) {
 
-        return null;
+        log.info("Iniciado serviço para atualizar Notícia.");
+
+        var response = Optional.ofNullable(noticia)
+            .map(this.noticiaUpdateOutputPort::update)
+            .orElseThrow(NoticiaUpdateUseCaseException::new);
+
+        log.info("Finalizado serviço para atualizar Notícia, com Id: {}.", response.getId());
+
+        return response;
     }
 }
 
