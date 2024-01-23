98dae0134a95a2d0112c0a1f3e97085aef8a356c
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-23 10:56:56 -0400

    TEST: add Adapter

--- micronoticias/src/test/java/microservice/micronoticias/adapter/out/NoticiaUpdateAdapterIntegrationTest.java
+++ micronoticias/src/test/java/microservice/micronoticias/adapter/out/NoticiaUpdateAdapterIntegrationTest.java
@@ -4,11 +4,13 @@ import microservice.micronoticias.adapter.out.entity.EditoriaEntity;
 import microservice.micronoticias.adapter.out.entity.NoticiaEntity;
 import microservice.micronoticias.adapter.out.repository.EditoriaRepository;
 import microservice.micronoticias.adapter.out.repository.NoticiaRepository;
+import microservice.micronoticias.application.core.domain.Editoria;
 import microservice.micronoticias.application.port.output.NoticiaUpdateOutputPort;
 import microservice.micronoticias.utility.FactoryObjectMother;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
@@ -65,11 +67,8 @@ class NoticiaUpdateAdapterIntegrationTest {
             Assertions.assertEquals("Fonte 1", noticiaEntity.getFontes().get(0));
             Assertions.assertEquals("Autor 1", noticiaEntity.getAutorias().get(0));
 
-            var editoriaDoBanco = noticiaEntity.getEditorias().stream().findFirst().orElseThrow();
             var editoria = factory.gerarEditoria();
-            editoria.setId(editoriaDoBanco.getId());
-            editoria.setNomenclatura(editoriaDoBanco.getNomenclatura());
-            editoria.setDescricao(editoriaDoBanco.getDescricao());
+            BeanUtils.copyProperties(editoriaEntity, editoria);
 
             var noticia = factory.gerarNoticia(30, 150, 250, 400, 5000, 1, 50, 1, 100);
             noticia.setEditorias(Set.of(editoria));
@@ -99,8 +98,8 @@ class NoticiaUpdateAdapterIntegrationTest {
     class UpdateEditoria {
 
         @Test
-        @DisplayName("dados válidos")
-        void dadoNoticiaValida_QuandoUpdateDaEditoria_EntaoRetornarNoticiaComEditoriaAtualizada() {
+        @DisplayName("um item - existente")
+        void dadoNoticiaValidaComUmaEditoria_QuandoUpdateDaEditoria_EntaoRetornarNoticiaComEditoriaAtualizada() {
             Assertions.assertEquals("Economia", editoriaEntity.getNomenclatura());
             Assertions.assertEquals("Informações sobre a economia do país e mundo.", editoriaEntity.getDescricao());
 
@@ -110,14 +109,7 @@ class NoticiaUpdateAdapterIntegrationTest {
             editoria.setDescricao("Informações atualizadas sobre a economia do país e do mundo.");
 
             var noticia = factory.gerarNoticia(30, 150, 250, 400, 5000, 1, 50, 1, 100);
-            noticia.setId(noticiaEntity.getId());
-            noticia.setChapeu(noticiaEntity.getChapeu());
-            noticia.setTitulo(noticiaEntity.getTitulo());
-            noticia.setLinhaFina(noticiaEntity.getLinhaFina());
-            noticia.setLide(noticiaEntity.getLide());
-            noticia.setCorpo(noticiaEntity.getCorpo());
-            noticia.setFontes(noticiaEntity.getFontes());
-            noticia.setAutorias(noticiaEntity.getAutorias());
+            BeanUtils.copyProperties(noticiaEntity, noticia);
             noticia.setEditorias(Set.of(editoria));
 
             var noticiaSalva = noticiaUpdateOutputPort.update(noticia);
@@ -125,6 +117,45 @@ class NoticiaUpdateAdapterIntegrationTest {
 
             Assertions.assertNotEquals("Economia", editoriaSalva.getNomenclatura());
             Assertions.assertNotEquals("Informações sobre a economia do país e mundo.", editoriaSalva.getDescricao());
+        }
+
+        @Test
+        @DisplayName("dois itens - um novo e um existente")
+        void dadoNoticiaValidaComDuasEditorias_QuandoUpdateDaEditoria_EntaoRetornarNoticiaComEditoriasAtualizadas() {
+            Assertions.assertEquals("Economia", editoriaEntity.getNomenclatura());
+            Assertions.assertEquals("Informações sobre a economia do país e mundo.", editoriaEntity.getDescricao());
+
+            var editoriaExiste = new Editoria();
+            editoriaExiste.setId(editoriaEntity.getId());
+            editoriaExiste.setNomenclatura("Economia atualizada");
+            editoriaExiste.setDescricao("Informações atualizadas sobre a economia do país e do mundo.");
+
+            var editoriaNova = factory.gerarEditoria();
+
+            var noticia = factory.gerarNoticia(30, 150, 250, 400, 5000, 1, 50, 1, 100);
+            BeanUtils.copyProperties(noticiaEntity, noticia);
+            noticia.setEditorias(Set.of(editoriaExiste, editoriaNova));
+
+            var noticiaSalva = noticiaUpdateOutputPort.update(noticia);
+
+            int atributosEncontrados = 0;
+
+            for (Editoria editoria : noticiaSalva.getEditorias()) {
+                if (editoria.getNomenclatura().equals(editoriaExiste.getNomenclatura())) {
+                    atributosEncontrados++;
+                }
+
+                if (editoria.getDescricao().equals(editoriaExiste.getDescricao())) {
+                    atributosEncontrados++;
+                }
+
+                if (atributosEncontrados == 2) {
+                    break;
+                }
+            }
+
+            Assertions.assertEquals(2, noticiaSalva.getEditorias().size());
+            Assertions.assertEquals(2, atributosEncontrados);
         }
     }
 }
