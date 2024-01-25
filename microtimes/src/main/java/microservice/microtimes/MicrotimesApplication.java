e232121d9e0e5e77fad3a161edb003363be7343b
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-25 11:33:29 -0400

    CHORE: config padrão micro

---
+++ microtimes/src/main/java/microservice/microtimes/MicrotimesApplication.java
@@ -0,0 +1,13 @@
+package microservice.microtimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrotimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrotimesApplication.class, args);
+	}
+
+}
