d02e5095724f6ba1d9109a815629ebcb8143a75e
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-30 16:13:09 -0400

    CHORE: config Kafka e Docker

--- microtorneios/src/main/java/microservice/microtorneios/adapters/in/consumer/ReceiveCreatedTimeConsumer.java
+++ microtorneios/src/main/java/microservice/microtorneios/adapters/in/consumer/ReceiveCreatedTimeConsumer.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import microservice.microtorneios.adapters.out.message.TimeMessage;
 import microservice.microtorneios.application.core.domain.enums.TimeEventEnum;
 import microservice.microtorneios.application.port.input.TimeInventoryCreateInputPort;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -15,15 +17,21 @@ public class ReceiveCreatedTimeConsumer {
 
     private final TimeInventoryCreateInputPort timeInventoryCreateInputPort;
 
-    @KafkaListener(topics = {"tp-saga-time"}, groupId = "time-inventory")
-    public void receive(TimeMessage timeMessage) {
+    @Value("${spring.kafka.group-id}")
+    private String groupId;
 
-        if (TimeEventEnum.CREATED_TIME.equals(timeMessage.getEvent())) {
+    @Value("${topic.name}")
+    private String topic;
+
+    @KafkaListener(topics = "${topic.name}", groupId = "${spring.kafka.group-id}", containerFactory = "kafkaListenerContainerFactory")
+    public void receive(ConsumerRecord<String, TimeMessage> record) {
+
+        if (TimeEventEnum.CREATED_TIME.equals(record.value().getEvent())) {
             log.info("Início do registro de Time criado.");
 
-            this.timeInventoryCreateInputPort.create(timeMessage.getTime());
+            this.timeInventoryCreateInputPort.create(record.value().getTime());
 
-            log.info("Registro bem-sucedido de Time, com nomeFantasia: {}", timeMessage.getTime().getNomeFantasia());
+            log.info("Registro bem-sucedido de Time, com nomeFantasia: {}", record.value().getTime().getNomeFantasia());
         }
     }
 }
--- microtorneios/src/main/java/microservice/microtorneios/config/kafka/KafkaConsumerConfig.java
+++ microtorneios/src/main/java/microservice/microtorneios/config/kafka/KafkaConsumerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,15 +22,19 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     public String bootstrapServers;
 
+    @Value("${spring.kafka.group-id}")
+    private String groupId;
+
     @Bean
     public ConsumerFactory<String, TimeMessage> consumerFactory() {
 
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, CustomDeserializer.class);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        return new DefaultKafkaConsumerFactory<>(props);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(TimeMessage.class, false));
     }
 
     @Bean
