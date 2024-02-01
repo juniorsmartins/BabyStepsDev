34326bdb21b070fc011f10b8deb15825d113cc40
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-02-01 17:36:59 -0400

    FEAT: add config tópicos Kafka

--- microinscricoes/src/main/java/microservice/microinscricoes/config/kafka/KafkaConfig.java
+++ microinscricoes/src/main/java/microservice/microinscricoes/config/kafka/KafkaConfig.java
@@ -1,6 +1,7 @@
 package microservice.microinscricoes.config.kafka;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
@@ -19,6 +21,10 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class KafkaConfig {
 
+    private static final Integer PARTITION_COUNT = 1;
+
+    private static final Integer REPLICA_COUNT = 1;
+
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
@@ -27,6 +33,12 @@ public class KafkaConfig {
 
     @Value("${spring.kafka.consumer.auto-offset-reset}")
     private String autoOffsetReset;
+
+    @Value("${spring.kafka.topic.start-saga}")
+    private String startSagaTopic;
+
+    @Value("${spring.kafka.topic.notify-ending}")
+    private String notifyEndingTopic;
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
@@ -63,6 +75,25 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public NewTopic startSagaTopic() {
+        return buildTopic(startSagaTopic);
+    }
+
+    @Bean
+    public NewTopic notifyEndingTopic() {
+        return buildTopic(notifyEndingTopic);
+    }
+
+    private NewTopic buildTopic(String name) {
+
+        return TopicBuilder
+            .name(name)
+            .replicas(REPLICA_COUNT)
+            .partitions(PARTITION_COUNT)
+            .build();
     }
 }
 
