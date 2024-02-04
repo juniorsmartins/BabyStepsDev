ba869ed08a4ad92525b8e5137cf159a6fa14a507
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-02-04 15:17:07 -0400

    FEAT: add exception

---
+++ orchestrator/src/main/java/microservice/orchestrator/config/exception/ExceptionDetails.java
@@ -0,0 +1,4 @@
+package microservice.orchestrator.config.exception;
+
+public record ExceptionDetails(int status, String message) { }
+
---
+++ orchestrator/src/main/java/microservice/orchestrator/config/exception/ExceptionGlobalHandler.java
@@ -0,0 +1,21 @@
+package microservice.orchestrator.config.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionGlobalHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<?> handleValidationException(ValidationException validationException) {
+
+        var details = new ExceptionDetails(HttpStatus.BAD_REQUEST.value(), validationException.getMessage());
+
+        return ResponseEntity
+            .badRequest()
+            .body(details);
+    }
+}
+
