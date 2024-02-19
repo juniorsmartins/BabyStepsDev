681f78cdfad621ecbcc41650ab4ae3967f11efa4
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-02-19 13:19:56 -0400

    TEST: add

--- microinscricoes/src/test/java/microservice/microinscricoes/adapter/in/controller/InscricaoControllerTest.java
+++ microinscricoes/src/test/java/microservice/microinscricoes/adapter/in/controller/InscricaoControllerTest.java
@@ -1,6 +1,5 @@
 package microservice.microinscricoes.adapter.in.controller;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
@@ -8,22 +7,18 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
 import microservice.microinscricoes.adapter.in.dto.TorneioIdDto;
-import microservice.microinscricoes.adapter.in.dto.request.InscricaoFiltroDto;
-import microservice.microinscricoes.adapter.in.dto.request.InscricaoOpenDtoIn;
 import microservice.microinscricoes.adapter.in.dto.response.InscricaoOpenDtoOut;
 import microservice.microinscricoes.adapter.out.repository.InscricaoRepository;
 import microservice.microinscricoes.adapter.out.repository.TorneioRepository;
+import microservice.microinscricoes.adapter.out.repository.entity.InscricaoEntity;
 import microservice.microinscricoes.adapter.out.repository.entity.TorneioEntity;
 import microservice.microinscricoes.utility.AbstractIntegrationTest;
 import microservice.microinscricoes.utility.FactoryObjectMother;
-import microservice.microinscricoes.utility.InscricaoOpenDtoPage;
 import microservice.microinscricoes.utility.TestConfigs;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.DeserializationFeature;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -49,24 +44,29 @@ class InscricaoControllerTest extends AbstractIntegrationTest {
     @Autowired
     private TorneioRepository torneioRepository;
 
-    private InscricaoOpenDtoIn inscricaoOpenDtoIn;
+    private InscricaoEntity inscricaoSalva;
 
-    private TorneioEntity torneioEntity1;
+    private TorneioEntity torneioSalvo2;
 
     @BeforeEach
     void setUp() {
         objectMapper = new ObjectMapper();
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES); // Usar somente nos testes para manter a segurança da API - Isso é usado quanto temos Hateoas
 
-        var torneioIdDto = new TorneioIdDto(1L);
-        this.inscricaoOpenDtoIn = this.factory.gerarInscricaoOpenDtoInBuilder()
-            .torneio(torneioIdDto)
-            .build();
-
-        torneioEntity1 = this.factory.gerarTorneioEntityBuilder()
+        var torneioEntity1 = this.factory.gerarTorneioEntityBuilder()
             .id(1L)
             .build();
         this.torneioRepository.save(torneioEntity1);
+
+        var inscricaoEntity1 = factory.gerarInscricaoEntityBuilder()
+                .torneio(torneioEntity1)
+                .build();
+        inscricaoSalva = inscricaoRepository.save(inscricaoEntity1);
+
+        var torneioEntity2 = factory.gerarTorneioEntityBuilder()
+                .id(2L)
+                .build();
+        torneioSalvo2 = torneioRepository.save(torneioEntity2);
 
         requestSpecification = new RequestSpecBuilder()
             .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_BABYSTEPS)
@@ -84,12 +84,17 @@ class InscricaoControllerTest extends AbstractIntegrationTest {
     }
 
     @Nested
-    @DisplayName("Post")
-    class Post {
+    @DisplayName("PostOpen")
+    class PostOpen {
 
         @Test
         @DisplayName("persistência")
         void dadoInscricaoOpenDtoInValido_QuandoOpen_EntaoRetornarDadosPersistidos() throws IOException {
+
+            var torneioIdDto = new TorneioIdDto(1L);
+            var inscricaoOpenDtoIn = factory.gerarInscricaoOpenDtoInBuilder()
+                .torneio(torneioIdDto)
+                .build();
 
             var response = RestAssured
                 .given().spec(requestSpecification)
@@ -117,25 +122,15 @@ class InscricaoControllerTest extends AbstractIntegrationTest {
     }
 
     @Nested
-    @DisplayName("Pesquisar")
-    class Pesquisar {
+    @DisplayName("GetPesquisar")
+    class GetPesquisar {
 
         @Test
         @DisplayName("http 200")
         void dadoGetValido_QuandoPesquisar_EntaoRetornarHttp200() throws IOException {
 
-            var inscricaoEntity1 = factory.gerarInscricaoEntityBuilder()
-                .torneio(torneioEntity1)
-                .build();
-            inscricaoRepository.save(inscricaoEntity1);
-
-            var torneioEntity2 = factory.gerarTorneioEntityBuilder()
-                .id(2L)
-                .build();
-            torneioRepository.save(torneioEntity2);
-
             var inscricaoEntity2 = factory.gerarInscricaoEntityBuilder()
-                .torneio(torneioEntity2)
+                .torneio(torneioSalvo2)
                 .build();
             inscricaoRepository.save(inscricaoEntity2);
 
@@ -154,23 +149,10 @@ class InscricaoControllerTest extends AbstractIntegrationTest {
         @DisplayName("filtro por Id")
         void dadoGetValido_QuandoPesquisarPorId_EntaoRetornarUmItem() throws IOException {
 
-            var inscricaoEntity1 = factory.gerarInscricaoEntityBuilder()
-                .torneio(torneioEntity1)
-                .build();
-            inscricaoRepository.save(inscricaoEntity1);
-
-            var torneioEntity2 = factory.gerarTorneioEntityBuilder()
-                .id(2L)
-                .build();
-            torneioRepository.save(torneioEntity2);
-
             var inscricaoEntity2 = factory.gerarInscricaoEntityBuilder()
-                .torneio(torneioEntity2)
+                .torneio(torneioSalvo2)
                 .build();
-            var inscricaoSalva = inscricaoRepository.save(inscricaoEntity2);
-
-            var filtro = new InscricaoFiltroDto();
-            filtro.setId("2");
+            inscricaoRepository.save(inscricaoEntity2);
 
             RestAssured
                 .given().spec(requestSpecification)
