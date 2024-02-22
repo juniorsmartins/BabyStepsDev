5cfefb72e360cd9b81370b74350b6fca213ae407
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-02-22 16:09:30 -0400

    TEST: add

--- microtimes/src/test/java/microservice/microtimes/adapter/in/controller/TimeControllerTest.java
+++ microtimes/src/test/java/microservice/microtimes/adapter/in/controller/TimeControllerTest.java
@@ -1,15 +1,21 @@
 package microservice.microtimes.adapter.in.controller;
 
 import microservice.microtimes.utility.AbstractTestcontainersTest;
+import microservice.microtimes.utility.ConverterUtilTest;
+import microservice.microtimes.utility.FactoryObjectMother;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -19,6 +25,8 @@ class TimeControllerTest extends AbstractTestcontainersTest {
     public static final String BASE_PATH = "/api/v1/times";
 
     public static final String UTF_8 = "UTF-8";
+
+    private final FactoryObjectMother factory = FactoryObjectMother.singleton();
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,6 +39,21 @@ class TimeControllerTest extends AbstractTestcontainersTest {
     @AfterEach
     void tearDown() {
 
+    }
+
+    @Test
+    @Order(1)
+    void dadoDadosValidos_quandoPostTime_entaoRetornarHttp201() throws Exception {
+
+        var time = this.factory.gerarTimeCreateDtoRequestBuilder().build();
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post(BASE_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(UTF_8)
+                .content(ConverterUtilTest.converterObjetoParaJson(time))
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isCreated())
+            .andDo(MockMvcResultHandlers.print());
     }
 }
 
---
+++ microtimes/src/test/java/microservice/microtimes/utility/ConverterUtilTest.java
@@ -0,0 +1,17 @@
+package microservice.microtimes.utility;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class ConverterUtilTest {
+
+    public static byte[] converterObjetoParaJson(Object object) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.registerModule(new JavaTimeModule());
+        return mapper.writeValueAsBytes(object);
+    }
+}
+
