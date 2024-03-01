edeb04c2a0d5ae882d186a9527b76e2329008c0f
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-03-01 18:44:12 -0400

    FEAT: internacionalização de mensagens

---
+++ microinscricoes/src/main/java/microservice/microinscricoes/config/exception/MessagesDefault.java
@@ -0,0 +1,16 @@
+package microservice.microinscricoes.config.exception;
+
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+//@RequiredArgsConstructor
+public final class MessagesDefault {
+
+    @Value("${inscricao.nao.encontrada}")
+    private String inscricao_nao_encontrada;
+}
+
--- microinscricoes/src/main/java/microservice/microinscricoes/config/exception/http_404/InscricaoNotFoundException.java
+++ microinscricoes/src/main/java/microservice/microinscricoes/config/exception/http_404/InscricaoNotFoundException.java
@@ -1,5 +1,8 @@
 package microservice.microinscricoes.config.exception.http_404;
 
+import lombok.RequiredArgsConstructor;
+import microservice.microinscricoes.config.exception.MessagesDefault;
+
 import java.io.Serial;
 
 public final class InscricaoNotFoundException extends RecursoNotFoundException {
@@ -11,8 +14,13 @@ public final class InscricaoNotFoundException extends RecursoNotFoundException {
     super(message);
   }
 
+//  public InscricaoNotFoundException(final Long id) {
+//    this(String.format("A Inscrição %d não foi encontrada.", id));
+//  }
+
   public InscricaoNotFoundException(final Long id) {
-    this(String.format("A Inscrição %d não foi encontrada.", id));
+    this(String.format(new MessagesDefault().getInscricao_nao_encontrada(), id));
+//    this(MessagesDefault.inscricao_nao_encontrada + " " + id);
   }
 }
 
