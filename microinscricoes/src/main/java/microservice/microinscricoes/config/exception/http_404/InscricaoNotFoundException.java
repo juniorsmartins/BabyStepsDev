ec6cf46cf17f048961460f77dea91b0338566bba
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-03-03 09:14:52 -0400

    FEAT: internacionalização de mensagens

--- microinscricoes/src/main/java/microservice/microinscricoes/config/exception/http_404/InscricaoNotFoundException.java
+++ microinscricoes/src/main/java/microservice/microinscricoes/config/exception/http_404/InscricaoNotFoundException.java
@@ -1,8 +1,5 @@
 package microservice.microinscricoes.config.exception.http_404;
 
-import lombok.RequiredArgsConstructor;
-import microservice.microinscricoes.config.exception.MessagesDefault;
-
 import java.io.Serial;
 
 public final class InscricaoNotFoundException extends RecursoNotFoundException {
@@ -14,13 +11,8 @@ public final class InscricaoNotFoundException extends RecursoNotFoundException {
     super(message);
   }
 
-//  public InscricaoNotFoundException(final Long id) {
-//    this(String.format("A Inscrição %d não foi encontrada.", id));
-//  }
-
   public InscricaoNotFoundException(final Long id) {
-    this(String.format(new MessagesDefault().getInscricao_nao_encontrada(), id));
-//    this(MessagesDefault.inscricao_nao_encontrada + " " + id);
+    this(String.format("A Inscrição %d não foi encontrada.", id));
   }
 }
 
