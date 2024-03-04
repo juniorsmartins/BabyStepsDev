a87da8238016bca0794af0652c6b2f0a233f75d3
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-03-04 17:37:49 -0400

    FEAT: saga pattern

--- gateway/src/main/java/microservice/gateway/GatewayApplication.java
+++ gateway/src/main/java/microservice/gateway/GatewayApplication.java
@@ -13,6 +13,8 @@ public class GatewayApplication {
 
 	private static final String MICROINSCRICOES = "lb://microinscricoes";
 
+	private static final String MICRONOTICIAS = "lb://micronoticias";
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
@@ -22,8 +24,8 @@ public class GatewayApplication {
 
 		return routeLocatorBuilder
 			.routes()
-				.route(rota -> rota.path("/api/v1/noticias/**").uri("lb://micronoticias")) // lb = Load Balance
-				.route(rota -> rota.path("/api/v1/editorias/**").uri("lb://micronoticias"))
+				.route(rota -> rota.path("/api/v1/noticias/**").uri(MICRONOTICIAS)) // lb = Load Balance
+				.route(rota -> rota.path("/api/v1/editorias/**").uri(MICRONOTICIAS))
 				.route(rota -> rota.path("/api/v1/persons/**").uri("lb://microusuarios"))
 				.route(rota -> rota.path("/api/v1/inscricoes/**").uri(MICROINSCRICOES))
 				.route(rota -> rota.path("/api/v1/inscritos/**").uri(MICROINSCRICOES))
