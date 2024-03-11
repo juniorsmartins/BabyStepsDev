72785c809dd8c62c03116d849efc59fff44d0fb1
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-03-11 17:44:59 -0400

    REFACTOR: microtorneios e microtimes

--- microinscricoes/src/main/java/microservice/microinscricoes/adapter/in/consumer/event/EventCreateTime.java
+++ microinscricoes/src/main/java/microservice/microinscricoes/adapter/in/consumer/event/EventCreateTime.java
@@ -2,13 +2,13 @@ package microservice.microinscricoes.adapter.in.consumer.event;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import microservice.microinscricoes.adapter.in.consumer.dto.TimeSaveDto;
+import microservice.microinscricoes.adapter.in.controller.dto.TimeIdDto;
 
 public record EventCreateTime(
 
     @NotNull
     @Valid
-    TimeSaveDto time
+    TimeIdDto time
 
 ) { }
 
