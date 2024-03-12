b5429725b5d9591a81dc7e42cc9d2a6baa29cf1c
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-03-12 16:11:53 -0400

    FEAT: saga pattern

--- microtorneios/src/main/java/microservice/microtorneios/config/usecase/SagaEventConfig.java
+++ microtorneios/src/main/java/microservice/microtorneios/config/usecase/SagaEventConfig.java
@@ -5,6 +5,7 @@ import microservice.microtorneios.adapters.out.TorneioFindAdapter;
 import microservice.microtorneios.adapters.out.TorneioSaveAdapter;
 import microservice.microtorneios.adapters.out.producer.CarteiroNotifyOrchestratorProducer;
 import microservice.microtorneios.adapters.utils.JsonUtil;
+import microservice.microtorneios.application.core.usecase.SagaEventFailUseCase;
 import microservice.microtorneios.application.core.usecase.SagaEventSuccessUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +14,20 @@ import org.springframework.context.annotation.Configuration;
 public class SagaEventConfig {
 
     @Bean
-    public SagaEventSuccessUseCase sagaEventUseCase(MapperIn mapperIn, JsonUtil jsonUtil,
+    public SagaEventSuccessUseCase sagaEventSuccessUseCase(MapperIn mapperIn, JsonUtil jsonUtil,
                                                     TorneioFindAdapter torneioFindAdapter,
                                                     TorneioSaveAdapter torneioSaveAdapter,
                                                     CarteiroNotifyOrchestratorProducer carteiroNotifyOrchestratorProducer) {
         return new SagaEventSuccessUseCase(mapperIn, jsonUtil, torneioFindAdapter, torneioSaveAdapter,
+                carteiroNotifyOrchestratorProducer);
+    }
+
+    @Bean
+    public SagaEventFailUseCase sagaEventFailUseCase(MapperIn mapperIn, JsonUtil jsonUtil,
+                                                 TorneioFindAdapter torneioFindAdapter,
+                                                 TorneioSaveAdapter torneioSaveAdapter,
+                                                 CarteiroNotifyOrchestratorProducer carteiroNotifyOrchestratorProducer) {
+        return new SagaEventFailUseCase(mapperIn, jsonUtil, torneioFindAdapter, torneioSaveAdapter,
                 carteiroNotifyOrchestratorProducer);
     }
 }
